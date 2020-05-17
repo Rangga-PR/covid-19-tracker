bba4aa70a7a161d9c391fbb1744509d1530ff459
Rangga-PR <ranggaputraraharja@gmail.com>
2020-05-18 00:53:32 +0700

    remove unnecesary element

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,8 @@ import Stats from "./component/stats";
 const App = () => {
   return (
     <>
-      <div className="content-wrapper">
-        <Title />
-        <Stats />
-      </div>
-      <style jsx>
-        {`
-          .content-wrapper {
-            max-width: 100%;
-            max-height: 100%;
-            height: 100%;
-            display: flex;
-            justify-content: space-between;
-            overflow-x: scroll;
-            ::-webkit-scrollbar {
-              display: none;
-            }
-            scrollbar-width: none;
-            -ms-overflow-style: none;
-            scroll-snap-type: x mandatory;
-          }
-        `}
-      </style>
+      <Title />
+      <Stats />
     </>
   );
 };
